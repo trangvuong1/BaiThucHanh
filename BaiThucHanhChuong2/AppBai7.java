6ce3396fe54b76e9d50831e89999cd330f1e57a4
vuongtrang1 <97450410+vuongtrang1@users.noreply.github.com>
2022-03-07 18:11:09 +0700

    add file java

---
+++ BaiThucHanhChuong2/AppBai7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AppBai7 {
+    public static void main(String[] args)  {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("20 so nguyen duong dau tien la:");
+        for(int i=0; i<=20; i++){
+            System.out.println(i);
+        }
+    }
+}
